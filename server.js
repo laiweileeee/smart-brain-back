eddb6d97fc07dfc4cfc01d83dedd7d08a7d572ec
Lai Wei Lee <laiwei>
2020-05-28 22:56:36 +0800

    .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: 'postgresql-amorphous-29185',
+        connectionString: process.env.DATABASE_URL,
         ssl: true,
     }
 });
